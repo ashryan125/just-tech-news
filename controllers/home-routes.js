1f617d6c4e37b4ecefcb25a7826864da329dfcd9
ashryan <20115553+ashryan125@users.noreply.github.com>
2021-07-16 18:11:41 -0400

    corrected html for login logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,8 +4,7 @@ const { Post, User, Comment, Vote } = require('../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
-  console.log(req.session);
-
+  console.log('======================');
   Post.findAll({
     attributes: [
       'id',
@@ -40,7 +39,6 @@ router.get('/', (req, res) => {
     });
 });
 
-// renders login/signup page
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
